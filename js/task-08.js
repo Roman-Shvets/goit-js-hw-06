0e5d010f5bf13043bec1485d04b3da96887ded95
Roman-Shvets <rshvets72@gmail.com>
2023-02-01 11:38:57 +0200

    Виправлена помилка task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,20 @@
 const formEl = document.querySelector('.login-form');
+const formData = { email:'', password:'' };
 
 formEl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
 event.preventDefault();
-
 const {elements: {email, password}} = event.currentTarget;
 
 if (email.value === "" || password.value === "") {
 return alert("Поля форми повинні бути заповнені");
 }
 
-console.log("Об'єкт із введеними даними:", event);
-console.log("Email:", event.currentTarget.elements.email.value);
-console.log("Password:", event.currentTarget.elements.password.value);
+formData.email = event.currentTarget.elements.email.value;
+formData.password = event.currentTarget.elements.password.value;
 
+console.log(formData);
+    
 event.currentTarget.reset();
 }
